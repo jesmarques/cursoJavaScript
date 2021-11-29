11114b3a4c85fb318c5a1ea8fde0c05717c05406
Jéssica Marques <jessicamarquesanjos@gmail.com>
2021-11-29 13:05:28 -0300

    Exemplos da aula 1 de JavaScript

---
+++ type_number.js
@@ -0,0 +1,23 @@
+// Tipo número
+
+const meuNumero = 3
+
+const primeiroNumero = 1
+const segundoNumero = 2
+
+const operacaoMatematica = primeiroNumero + segundoNumero
+
+console.log(operacaoMatematica)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
